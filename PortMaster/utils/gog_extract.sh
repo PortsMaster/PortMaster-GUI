caf18353f4fc873edf549e1ad3c35bbc0bdb9a2d
Ophir Doron <tulidoron@gmail.com>
2025-03-30 18:48:59 +0300

    add x86_64
    took em from ubuntu

--- PortMaster/utils/gog_extract.sh
+++ PortMaster/utils/gog_extract.sh
@@ -1,7 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
-# Set library path for aarch64
-export LD_LIBRARY_PATH="$(dirname "$0")/innoextract/libs.aarch64:$LD_LIBRARY_PATH"
+# Set the correct innoextract binary
+INNOEXTRACT="./PortMaster/utils/innoextract_$DEVICE_ARCH"
 
-# Call innoextract with passed arguments
-"$(dirname "$0")/innoextract/innoextract" "$@"
+# Check if the binary exists
+if [ ! -f "$INNOEXTRACT" ]; then
+    echo "Error: innoextract binary for $DEVICE_ARCH not found!"
+    exit 1
+fi
+
+# Set LD_LIBRARY_PATH
+export LD_LIBRARY_PATH="./PortMaster/utils/libs.$DEVICE_ARCH:$LD_LIBRARY_PATH"
+
+# Run innoextract with provided arguments
+$INNOEXTRACT "$@"
