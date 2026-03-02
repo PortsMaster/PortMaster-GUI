dbb53bc45a51e6875f4724c58d1dd1f4ef55ae86
Jacob <kloptops@gmail.com>
2026-03-02 15:05:22 +0800

    Added Esperanto, updated translations.

--- do_i18n.sh
+++ do_i18n.sh
@@ -7,7 +7,7 @@
 POT_DIR="PortMaster/pylibs/locales"
 POT_FILES=("messages" "themes")
 # Languages registered but not yet translated
-NOT_WORKING=("da_DK" "fi_FI")
+NOT_WORKING=("da_DK" "fi_FI" "eo_UY")
 
 for POT_FILE in "${POT_FILES[@]}"; do
     if [[ "$POT_FILE" == "messages" ]]; then
